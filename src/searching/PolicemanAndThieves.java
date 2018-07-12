f844578afbe46caabdd3caa82ed81ec4417fcf9a
ankitpandoh <pandoh.2007@gmail.com>
2018-07-13 00:56:08 +0530

    Solution of PoliceManAndThieves.java

---
+++ src/searching/PolicemanAndThieves.java
@@ -0,0 +1,195 @@
+package searching;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class PolicemanAndThieves {
+
+	public static void main(String[] args) {
+		InputReader in = new InputReader(System.in);
+		OutputWriter out = new OutputWriter(System.out);
+		int t = in.readInt();
+		while (t-- > 0) {
+			int n = in.readInt();
+			int k = in.readInt();
+			int j = n;
+			int count = 0;
+			while (j-- > 0) {
+				Queue<Integer> police = new LinkedList<>();
+				Queue<Integer> thieves = new LinkedList<>();
+				for (int i = 0; i < n; i++) {
+					if ("P".equals(in.readString())) {
+						police.add(i);
+					} else {
+						thieves.add(i);
+					}
+				}
+				while (police.size() > 0 && thieves.size() > 0) {
+					if (Math.abs(police.peek() - thieves.peek()) <= k) {
+						count++;
+						police.poll();
+						thieves.poll();
+					} else {
+						if (police.peek() > thieves.peek()) {
+							thieves.poll();
+						} else {
+							police.poll();
+						}
+					}
+				}
+			}
+			out.printLine(count);
+		}
+		out.close();
+	}
+
+	static String getString(String str, int length) {
+		if (length <= 1)
+			return str;
+		int mid = 0;
+		if (length % 2 == 0)
+			mid = length / 2 - 1;
+		else
+			mid = length / 2;
+		String leftStr = str.substring(0, mid);
+		String rightStr = str.substring(mid + 1, str.length());
+		String result = str.charAt(mid) + getString(leftStr, leftStr.length()) + getString(rightStr, rightStr.length());
+		return result;
+	}
+
+	static class InputReader {
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public String readLine() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isEndOfLine(c));
+			return res.toString();
+		}
+
+		public String readString() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isSpaceChar(c));
+			return res.toString();
+		}
+
+		public long readLong() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public int readInt() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public boolean isEndOfLine(int c) {
+			return c == '\n' || c == '\r' || c == -1;
+		}
+	}
+
+	static class OutputWriter {
+		private final PrintWriter writer;
+
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+
+		public void print(Object... objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0)
+					writer.print(' ');
+				writer.print(objects[i]);
+			}
+		}
+
+		public void printLine(Object... objects) {
+			print(objects);
+			writer.println();
+		}
+
+		public void close() {
+			writer.close();
+		}
+	}
+}
