62367ea5affe87cd4e7c862ea93a54df2dc6277a
ankitpandoh <pandoh.2007@gmail.com>
2018-08-03 12:52:15 +0530

    Solution to Problem Haunted

---
+++ src/heap/Haunted.java
@@ -0,0 +1,187 @@
+package heap;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class Haunted {
+
+	public static void main(String[] args) {
+		InputReader in = new InputReader(System.in);
+		OutputWriter out = new OutputWriter(System.out);
+		int n = in.readInt();
+		int m = in.readInt();
+		Map<Integer, Ghost> map = new HashMap<>();
+		PriorityQueue<Ghost> qu = new PriorityQueue<>(new Comparator<Ghost>() {
+			@Override
+			public int compare(Ghost o1, Ghost o2) {
+				if (o2.count > o1.count)
+					return 1;
+				else if (o1.count == o2.count)
+					return o2.age > o1.age ? 1 : -1;
+				else
+					return -1;
+			}
+		});
+		for (int i = 0; i < n; i++) {
+			int age = in.readInt();
+			Ghost ghost = map.get(age);
+			if (ghost == null) {
+				ghost = new Ghost(age,1);
+			} else {
+				ghost.count++;
+			}
+			map.put(age, ghost);
+			qu.add(ghost);
+			out.printLine(qu.peek().age + " " + qu.peek().count);
+		}
+		out.close();
+	}
+
+	static class Ghost {
+		int age;
+		int count;
+
+		Ghost(int age, int count) {
+			this.age = age;
+			this.count = count;
+		}
+	}
+
+	static class InputReader {
+		private InputStream stream;
+		private byte[] buf = new byte[1024];
+		private int curChar;
+		private int numChars;
+
+		public InputReader(InputStream stream) {
+			this.stream = stream;
+		}
+
+		public int read() {
+			if (numChars == -1)
+				throw new InputMismatchException();
+			if (curChar >= numChars) {
+				curChar = 0;
+				try {
+					numChars = stream.read(buf);
+				} catch (IOException e) {
+					throw new InputMismatchException();
+				}
+				if (numChars <= 0)
+					return -1;
+			}
+			return buf[curChar++];
+		}
+
+		public String readLine() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isEndOfLine(c));
+			return res.toString();
+		}
+
+		public String readString() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			StringBuilder res = new StringBuilder();
+			do {
+				res.appendCodePoint(c);
+				c = read();
+			} while (!isSpaceChar(c));
+			return res.toString();
+		}
+
+		public long readLong() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			long res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public int readInt() {
+			int c = read();
+			while (isSpaceChar(c))
+				c = read();
+			int sgn = 1;
+			if (c == '-') {
+				sgn = -1;
+				c = read();
+			}
+			int res = 0;
+			do {
+				if (c < '0' || c > '9')
+					throw new InputMismatchException();
+				res *= 10;
+				res += c - '0';
+				c = read();
+			} while (!isSpaceChar(c));
+			return res * sgn;
+		}
+
+		public boolean isSpaceChar(int c) {
+			return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+		}
+
+		public boolean isEndOfLine(int c) {
+			return c == '\n' || c == '\r' || c == -1;
+		}
+	}
+
+	static class OutputWriter {
+		private final PrintWriter writer;
+
+		public OutputWriter(OutputStream outputStream) {
+			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(outputStream)));
+		}
+
+		public OutputWriter(Writer writer) {
+			this.writer = new PrintWriter(writer);
+		}
+
+		public void print(Object... objects) {
+			for (int i = 0; i < objects.length; i++) {
+				if (i != 0)
+					writer.print(' ');
+				writer.print(objects[i]);
+			}
+		}
+
+		public void printLine(Object... objects) {
+			print(objects);
+			writer.println();
+		}
+
+		public void close() {
+			writer.close();
+		}
+	}
+}
