ab721360750936452e526de5d127a1c4648793c0
Ankit Pandoh <pandoh.2007@gmail.com>
2018-10-13 02:55:54 +0530

    updated for corner scenario

--- src/math/MicroAndEvenNumbers.py
+++ src/math/MicroAndEvenNumbers.py
@@ -4,4 +4,7 @@ n, k = map(int, input().split(" "))
 arr = list(map(int, input().split(" ")))
 arr = [x for x in arr if x%2 == 0]
 l = len(arr)
-print(int(m.factorial(l)/(m.factorial(k) * m.factorial(l-k))))
+if l-k < 0:
+    print(0)
+else:
+    print(int(m.factorial(l)/(m.factorial(k) * m.factorial(l-k))))
